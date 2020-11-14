acde585fe43f9eee7178a6846339651e0499cc6b
Felipe <felipe.svfx@gmail.com>
2020-11-14 10:27:44 -0300

    create configs and templates to use TS

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,26 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
-}
+  siteMetadata: {
+    title: `2Lipe Dev Blog`,
+    position: `Software Developer`,
+    description: `Um blog sobre desenvolvimento, curiosidades e muito mais sobre o mundo da técnologia`,
+    author: `Felipe Vieira`,
+    siteUrl: `https://linkedin.com/felipesvfx`,
+  },
+  plugins: [
+    `gatsby-plugin-typescript`,
+    {
+      resolve: `gatsby-plugin-eslint`,
+      options: {
+        test: /\.js$|\.jsx$|\.ts$|\.tsx$/,
+        exclude: /(node_modules|.cache|public)/,
+        stages: ['develop'],
+        options: {
+          emitWarning: true,
+          failOnError: false,
+        },
+      },
+    },
+  ],
+};
